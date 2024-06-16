2716c30674de456cc06dc91a5d348057a42577ef
admin <admin@admins-MBP.lan>
2024-06-16 12:18:24 -0500

    Added all the routes in videoRoutes

--- backend/routes/videoRoutes.js
+++ backend/routes/videoRoutes.js
@@ -1,3 +1,19 @@
 import express from 'express';
 
+const router = express.Router();
+import {
+    addVideo,
+    getVideos,
+    editVideo,
+    getVideo,
+    commentVideo,
+    getComments
+} from '../controllers/videoController.js';
+
+router.route('/').post(addVideo).get(getVideos).put(editVideo);
+router.route('/single').get(getVideo);
+router.route('/comments').post(commentVideo).get(getComments)
+
+
+export default router;
 
